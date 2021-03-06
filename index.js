c9f044ecb8836a023f9342ab769c6417892cfd13
James Nowecki <jamesnowecki@gmail.com>
2021-03-06 14:33:33 +0000

    added guid funcs from stackoverflow

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ wsServer.on('request', request => {
     })
 
 })
+
+const S4 = () => {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+ 
+// Generate a guid (Stolen from stack overflow)
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
